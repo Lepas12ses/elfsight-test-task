2add43dd458858460bbbc69a3b7cdbf72a2f79a4
Amogus <nomail@gus.com>
2025-11-18 18:14:49 +0300

    Added width prop to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 
-export function Button({ children, color }) {
-  return <StyledButton _color={color}>{children}</StyledButton>;
+export function Button({ children, color, width }) {
+  return (
+    <StyledButton _width={width} _color={color}>
+      {children}
+    </StyledButton>
+  );
 }
 
 const StyledButton = styled.button`
   font-size: 16px;
   color: ${({ _color }) => _color};
   padding: 12px;
-  width: fit-content;
+  width: ${({ _width }) => _width};
   border-radius: 8px;
   border: 1px solid ${({ _color }) => _color};
   background: none;
